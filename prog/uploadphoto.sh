4f52945a4f1d610b68a8003e4ee157f529da2563
Nathaniut <nathan.guillaud@orange.fr>
2017-12-21 17:17:54 +0000

    Enregistrement qui fonctionne sudo git add uploadphotomessage.sh!

--- prog/uploadphoto.sh
+++ prog/uploadphoto.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-./gdrive upload "$1"
+./gdrive mkdir "$2"
+id=`./gdrive list -q "name contains '$2'" | sed '1d' | cut -d " " -f 1`
 
+./gdrive upload -p "$id" "$1"
--- prog/uploadphotomessage.sh
+++ prog/uploadphotomessage.sh
@@ -1,3 +1,7 @@
-#!/bin/bash
-./gdrive upload "$1"
 
+#!/bin/bash
+./gdrive mkdir "$3"
+id=`./gdrive list -q "name contains '$3'" | sed '1d' | cut -d " " -f 1`
+
+./gdrive upload -p "$id" "$1"
+./gdrive upload -p "$id" "$2"
